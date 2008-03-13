04bde0de386263154575e04529d00dc81228b1bc
Jim Safley <jimsafley@gmail.com>
2008-03-13 18:40:41 +0000

    added theme/ them directory and plugin.ini

---
+++ theme/csvimport/index.php
@@ -0,0 +1,13 @@
+<?php head(); ?>
+
+<div id="primary">
+
+<?php echo flash(); ?>
+
+<h2>Import from CSV to Omeka</h2>
+
+<p><a href="<?php echo uri('csvimport/index/form'); ?>">Begin the import process...</a></p>
+
+</div>
+
+<?php foot(); ?>
