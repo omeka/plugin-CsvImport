50714a61f030ceb5572d6d19ec02747a404e897d
Kristopher Kelly <kristopherbkelly@gmail.com>
2011-03-15 19:02:49 +0000

    Fixes js to allow use of jQuery $.

--- views/admin/javascripts/csv-import.js
+++ views/admin/javascripts/csv-import.js
@@ -8,27 +8,27 @@ Omeka.CsvImport = {};
  * Allow multiple mappings for each field, and add buttons to allow a mapping
  * to be removed.
  */
-Omeka.CsvImport.enableElementMapping = function () {
-    jQuery('form#csvimport .map-element').change(function () {
-        var select = jQuery(this);
+Omeka.CsvImport.enableElementMapping = function() {(function ($) {
+    $('form#csvimport .map-element').change(function () {
+        var select = $(this);
         var addButton = select.siblings('span.add-element');
         if (!addButton.length) {
-            var addButton = jQuery('<span class="add-element"></span>');
+            var addButton = $('<span class="add-element"></span>');
             addButton.click(function() {
                 var copy = select.clone(true);
                 select.after(copy);
-                jQuery(this).remove();
+                $(this).remove();
             });
             select.after(addButton);
         };
     });
-};
+})(jQuery)};
 
 /**
  * Add a confirm step before undoing an import.
  */
-Omeka.CsvImport.confirmUndoImport = function () {
-    jQuery('.csv-undo-import').click(function () {
+Omeka.CsvImport.confirmUndoImport = function() {(function ($) {
+    $('.csv-undo-import').click(function () {
         return confirm("Undoing an import will delete all of its imported items. Are you sure you want to undo this import?");
     });
-};
+})(jQuery)};
