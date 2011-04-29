625b2a827f56fcce261ccec57354c7c537be5bb4
Kristopher Kelly <kristopherbkelly@gmail.com>
2011-04-29 18:51:22 +0000

    Adds support for new tag_delimiter database option.

--- models/CsvImport/ColumnMap/Tag.php
+++ models/CsvImport/ColumnMap/Tag.php
@@ -1,6 +1,8 @@
 <?php
 class CsvImport_ColumnMap_Tag extends CsvImport_ColumnMap
 {
+    const DEFAULT_DELIMITER = ',';
+
     public function __construct($columnName)
     {
         parent::__construct($columnName);
@@ -9,10 +11,19 @@ class CsvImport_ColumnMap_Tag extends CsvImport_ColumnMap
 
     public function map($row, $result)
     {
-        $rawTags = explode(',', $row[$this->_columnName]);
+        $rawTags = explode($this->_getDelimiter(), $row[$this->_columnName]);
         $trimmed = array_map('trim', $rawTags);
         $cleaned = array_diff($trimmed, array(''));
         $tags = array_merge($result, $cleaned);
         return $tags;
+    }
+
+    private function _getDelimiter()
+    {
+        // New in 1.4.
+        if (!($delimiter = get_option('tag_delimiter'))) {
+            $delimiter = self::DEFAULT_DELIMITER;
+        }
+        return $delimiter;
     }
 }
