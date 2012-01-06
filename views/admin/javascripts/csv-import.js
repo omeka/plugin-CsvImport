eb64dcb999248559679e9732afe308ba124060af
John Flatness <john@zerocrates.org>
2012-01-06 13:22:37 -0500

    Shrink csv-import.js.

--- views/admin/javascripts/csv-import.js
+++ views/admin/javascripts/csv-import.js
@@ -1,45 +1,43 @@
-if (typeof Omeka === 'undefined') {
-    Omeka = {};
+if (!Omeka) {
+    var Omeka = {};
 }
 
 Omeka.CsvImport = {};
 
-/**
- * Allow multiple mappings for each field, and add buttons to allow a mapping
- * to be removed.
- */
-Omeka.CsvImport.enableElementMapping = function() {(function ($) {
-    $('form#csvimport .map-element').change(function () {
-        var select = $(this);
-        var addButton = select.siblings('span.add-element');
-        if (!addButton.length) {
-            var addButton = $('<span class="add-element"></span>');
-            addButton.click(function() {
-                var copy = select.clone(true);
-                select.after(copy);
-                $(this).remove();
-            });
-            select.after(addButton);
-        };
-    });
-})(jQuery)};
+(function ($) {
+    /**
+     * Allow multiple mappings for each field, and add buttons to allow a mapping
+     * to be removed.
+     */
+    Omeka.CsvImport.enableElementMapping = function () {
+        $('form#csvimport .map-element').change(function () {
+            var select = $(this);
+            var addButton = select.siblings('span.add-element');
+            if (!addButton.length) {
+                var addButton = $('<span class="add-element"></span>');
+                addButton.click(function() {
+                    var copy = select.clone(true);
+                    select.after(copy);
+                    $(this).remove();
+                });
+                select.after(addButton);
+            };
+        });
+    };
 
-/**
- * Add a confirm step before undoing an import.
- */
-Omeka.CsvImport.confirm = function() {(function ($) {
-    $('.csv-undo-import').click(function () {
-        return confirm("Undoing an import will delete all of its imported items. Are you sure you want to undo this import?");
-    });
-})(jQuery)};
+    /**
+     * Add a confirm step before undoing an import.
+     */
+    Omeka.CsvImport.confirm = function () {
+        $('.csv-undo-import').click(function () {
+            return confirm("Undoing an import will delete all of its imported items. Are you sure you want to undo this import?");
+        });
+    };
 
-
-/**
- * Disable most options if Import from Csv Report is checked
- * 
- */
-
-Omeka.CsvImport.toggleImportOptions = function () {(function ($) {
-	$('div.field').has('#item_type_id, #collection_id, #items_are_public, #items_are_featured, #column_delimiter').slideToggle();
-})(jQuery)};
-
+    /**
+     * Disable most options if Import from Csv Report is checked
+     */
+    Omeka.CsvImport.toggleImportOptions = function () {
+        $('div.field').has('#item_type_id, #collection_id, #items_are_public, #items_are_featured, #column_delimiter').slideToggle();
+    };
+})(jQuery);
