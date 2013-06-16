0bdefae43a887c723343429c41dd6007ddb066ad
Daniel Berthereau <Daniel.github@Berthereau.net>
2013-06-16 21:00:02 +0200

    Added readme file.

--- CsvImportPlugin.php
+++ CsvImportPlugin.php
@@ -1,59 +1,11 @@
 <?php
 /**
-* CsvImportPlugin class - represents the Csv Import plugin
-*
-* Configuring the plugin:  Set the proper settings in config.ini
-* like so:
-*
-* <code>
-* plugins.CsvImport.columnDelimiter = ","
-* plugins.CsvImport.memoryLimit = "128M"
-* plugins.CsvImport.requiredExtension = "txt"
-* plugins.CsvImport.requiredMimeType = "text/csv"
-* plugins.CsvImport.maxFileSize = "10M"
-* plugins.CsvImport.fileDestination = "/tmp"
-* plugins.CsvImport.batchSize = "1000"
-* </code>
-*
-* All of the above settings are optional.  If not given, CsvImport uses the
-* following default values:
-*
-* memoryLimit = current script limit
-* requiredExtension = "txt" or "csv"
-* requiredMimeType = "text/csv"
-* maxFileSize = current system upload limit
-* fileDestination = current system temporary dir (via sys_get_temp_dir())
-* batchSize = 0 (no batching)
-*
-*
-* Set a high memory limit to avoid memory allocation issues with imports.
-* Examples include 128M, 1G, and -1.  This will set PHP's memory_limit setting
-* directly, see PHP's documentation for more info on formatting this number.
-* Be advised that many web hosts set a maximum memory limit, so this setting
-* may be ignored if it exceeds the maximum allowable limit. Check with your web
-* host for more information.
-*
-* Note that 'maxFileSize' will not affect post_max_size or upload_max_filesize
-* as is set in php.ini.  Having a maxFileSize that exceeds either
-* will still result in errors that prevent the file upload.
-*
-* batchSize: Setting for advanced users.  If you find that your long-running
-* imports are using too much memory or otherwise hogging system resources,
-* set this value to split your import into multiple jobs based on the
-* number of CSV rows to process per job.
-*
-* For example, if you have a CSV with 150000 rows, setting a batchSize
-* of 5000 would cause the import to be split up over 30 separate jobs.
-* Note that these jobs run sequentially based on the results of prior
-* jobs, meaning that the import cannot be parallelized.  The first job
-* will import 5000 rows and then spawn the next job, and so on until
-* the import is completed.
-*
-*
-* @copyright Copyright 2008-2012 Roy Rosenzweig Center for History and New Media
-* @license http://www.gnu.org/licenses/gpl-3.0.txt GNU GPLv3
-* @package CsvImport
-*/
+ * CsvImportPlugin class - represents the Csv Import plugin
+ *
+ * @copyright Copyright 2008-2012 Roy Rosenzweig Center for History and New Media
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt GNU GPLv3
+ * @package CsvImport
+ */
 
 defined('CSV_IMPORT_DIRECTORY') or define('CSV_IMPORT_DIRECTORY', dirname(__FILE__));
 
