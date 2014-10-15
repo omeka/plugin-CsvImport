4aa8dc85662d4ac70fa4a54d4753f3ff185d9f2b
John Flatness <john@zerocrates.org>
2014-10-15 13:36:23 -0400

    Remove duplicate timestamp setting.

--- models/CsvImport/Import.php
+++ models/CsvImport/Import.php
@@ -461,7 +461,6 @@ class CsvImport_Import extends Omeka_Record_AbstractRecord
             return false;
         }
 
-        $this->added = date("Y-m-d H:i:s", time());
         $this->status = self::QUEUED;
         $this->save();
         $this->_log("Queued import.");
