1aa08367c49755108fdb6f3ac05473853d41ff32
Kim Nguyen <design@kimisgold.com>
2021-09-09 15:48:19 -0400

    Add responsive table wrapper.

--- views/admin/index/browse.php
+++ views/admin/index/browse.php
@@ -7,66 +7,68 @@
     <?php echo flash(); ?>
     <div class="pagination"><?php echo pagination_links(); ?></div>
     <?php if (iterator_count(loop('CsvImport_Import'))): ?>
-    <table class="simple" cellspacing="0" cellpadding="0">
-        <thead>
-            <tr>                
-                <?php
-                $browseHeadings[__('Import Date')] = 'added';
-                $browseHeadings[__('CSV File')] = 'original_filename';
-                $browseHeadings[__('Imported Items')] = null;
-                $browseHeadings[__('Skipped Items')] = 'skipped_item_count';
-                $browseHeadings[__('Skipped Rows')] = 'skipped_row_count';
-                $browseHeadings[__('Status')] = 'status';
-                $browseHeadings[__('Action')] = null;
-                echo browse_sort_links($browseHeadings, array('link_tag' => 'th scope="col"', 'list_tag' => '')); 
-                ?>
-            </tr>
-        </thead>
-        <tbody>            
-            <?php $key = 0; ?>
-            <?php foreach (loop('CsvImport_Import') as $csvImport): ?>
-            <tr class="<?php if(++$key%2==1) echo 'odd'; else echo 'even'; ?>">
-            
-                <td><?php echo html_escape(format_date($csvImport->added, Zend_Date::DATETIME_SHORT)); ?></td>
-                <td><?php echo html_escape($csvImport->original_filename); ?></td>
-                
-                <?php $importedItemCount = $csvImport->getImportedItemCount(); ?>
-                <td><?php echo html_escape($importedItemCount); ?></td>
-                <td><?php echo html_escape($csvImport->skipped_item_count); ?></td>
-                <td><?php echo html_escape($csvImport->skipped_row_count); ?></td>
-                
-                <td><?php echo html_escape(__(Inflector::humanize($csvImport->status, 'all'))); ?></td>
-                <?php
-                    if ($csvImport->isCompleted() 
-                        || $csvImport->isStopped()
-                        || ($csvImport->isImportError() && $importedItemCount > 0)): ?>
-                    <?php 
-                    $undoImportUrl = $this->url(array('action' => 'undo-import',
-                                                      'id' => $csvImport->id),
-                                                      'default'); 
+    <div class="table-responsive">
+        <table class="simple" cellspacing="0" cellpadding="0">
+            <thead>
+                <tr>                
+                    <?php
+                    $browseHeadings[__('Import Date')] = 'added';
+                    $browseHeadings[__('CSV File')] = 'original_filename';
+                    $browseHeadings[__('Imported Items')] = null;
+                    $browseHeadings[__('Skipped Items')] = 'skipped_item_count';
+                    $browseHeadings[__('Skipped Rows')] = 'skipped_row_count';
+                    $browseHeadings[__('Status')] = 'status';
+                    $browseHeadings[__('Action')] = null;
+                    echo browse_sort_links($browseHeadings, array('link_tag' => 'th scope="col"', 'list_tag' => '')); 
                     ?>
-                    <td>
-                        <a href="<?php echo html_escape($undoImportUrl);  ?>" class="csv-undo-import delete-button"><?php echo html_escape(__('Undo Import')); ?></a>
-                    </td>
-                <?php elseif ($csvImport->isUndone() || 
-                              $csvImport->isUndoImportError() || 
-                              $csvImport->isOtherError() || 
-                              ($csvImport->isImportError() && $importedItemCount == 0)): ?>
-                    <?php 
-                    $clearHistoryImportUrl = $this->url(array('action' => 'clear-history',
-                                                              'id' => $csvImport->id),
-                                                              'default'); 
-                    ?>
-                    <td>
-                        <a href="<?php echo html_escape($clearHistoryImportUrl);  ?>" class="csv-clear-history delete-button"><?php echo html_escape(__('Clear History')); ?></a>
-                    </td>
-                <?php else: ?>
-                    <td></td>
-                <?php endif; ?>
-            </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+                </tr>
+            </thead>
+            <tbody>            
+                <?php $key = 0; ?>
+                <?php foreach (loop('CsvImport_Import') as $csvImport): ?>
+                <tr class="<?php if(++$key%2==1) echo 'odd'; else echo 'even'; ?>">
+                
+                    <td><?php echo html_escape(format_date($csvImport->added, Zend_Date::DATETIME_SHORT)); ?></td>
+                    <td><?php echo html_escape($csvImport->original_filename); ?></td>
+                    
+                    <?php $importedItemCount = $csvImport->getImportedItemCount(); ?>
+                    <td><?php echo html_escape($importedItemCount); ?></td>
+                    <td><?php echo html_escape($csvImport->skipped_item_count); ?></td>
+                    <td><?php echo html_escape($csvImport->skipped_row_count); ?></td>
+                    
+                    <td><?php echo html_escape(__(Inflector::humanize($csvImport->status, 'all'))); ?></td>
+                    <?php
+                        if ($csvImport->isCompleted() 
+                            || $csvImport->isStopped()
+                            || ($csvImport->isImportError() && $importedItemCount > 0)): ?>
+                        <?php 
+                        $undoImportUrl = $this->url(array('action' => 'undo-import',
+                                                        'id' => $csvImport->id),
+                                                        'default'); 
+                        ?>
+                        <td>
+                            <a href="<?php echo html_escape($undoImportUrl);  ?>" class="csv-undo-import delete-button"><?php echo html_escape(__('Undo Import')); ?></a>
+                        </td>
+                    <?php elseif ($csvImport->isUndone() || 
+                                $csvImport->isUndoImportError() || 
+                                $csvImport->isOtherError() || 
+                                ($csvImport->isImportError() && $importedItemCount == 0)): ?>
+                        <?php 
+                        $clearHistoryImportUrl = $this->url(array('action' => 'clear-history',
+                                                                'id' => $csvImport->id),
+                                                                'default'); 
+                        ?>
+                        <td>
+                            <a href="<?php echo html_escape($clearHistoryImportUrl);  ?>" class="csv-clear-history delete-button"><?php echo html_escape(__('Clear History')); ?></a>
+                        </td>
+                    <?php else: ?>
+                        <td></td>
+                    <?php endif; ?>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
     <?php else: ?>
         <p><?php echo __('You have no imports yet.'); ?></p> 
     <?php endif; ?>
--- views/admin/index/map-columns-form.php
+++ views/admin/index/map-columns-form.php
@@ -3,28 +3,30 @@
     $colNames = $this->columnNames;
     $colExamples = $this->columnExamples;
 ?>
+    <div class="table-responsive">
     <table id="column-mappings" class="simple" cellspacing="0" cellpadding="0">
-    <thead>
-    <tr>
-        <th><?php __('Column'); ?></th>
-        <th><?php echo __('Example from CSV File'); ?></th>
-        <th><?php echo __('Map To Element'); ?></th>
-        <th><?php echo __('Use HTML?'); ?></th>
-        <th><?php echo __('Tags?'); ?></th>
-        <th><?php echo __('Files?'); ?></th>
-    </tr>
-    </thead>
-    <tbody>
-<?php for($i = 0; $i < count($colNames); $i++): ?>
+        <thead>
         <tr>
-        <td><strong><?php echo html_escape($colNames[$i]); ?></strong></td>
-        <?php $exampleString = $colExamples[$colNames[$i]]; ?>
-        <td><tt><?php echo html_escape(substr($exampleString, 0, 47)); ?><?php if (strlen($exampleString) > 47) { echo '&hellip;';} ?></tt></td>
-        <?php echo $this->form->getSubForm("row$i"); ?>
+            <th><?php __('Column'); ?></th>
+            <th><?php echo __('Example from CSV File'); ?></th>
+            <th><?php echo __('Map To Element'); ?></th>
+            <th><?php echo __('Use HTML?'); ?></th>
+            <th><?php echo __('Tags?'); ?></th>
+            <th><?php echo __('Files?'); ?></th>
         </tr>
-<?php endfor; ?>
-    </tbody>
-    </table>
+        </thead>
+        <tbody>
+        <?php for($i = 0; $i < count($colNames); $i++): ?>
+            <tr>
+            <td><strong><?php echo html_escape($colNames[$i]); ?></strong></td>
+            <?php $exampleString = $colExamples[$colNames[$i]]; ?>
+            <td><tt><?php echo html_escape(substr($exampleString, 0, 47)); ?><?php if (strlen($exampleString) > 47) { echo '&hellip;';} ?></tt></td>
+            <?php echo $this->form->getSubForm("row$i"); ?>
+            </tr>
+        <?php endfor; ?>
+        </tbody>
+        </table>
+    </div>
     <fieldset>
     <?php echo $this->form->submit; ?>
     </fieldset>
